caa0a894250072dcd5f81b5fb70057df75759d09
Paula <generalpaulawrk@gmail.com>
2025-12-27 17:58:50 -0300

    Test unitario de la función revisarCarpeta

--- tests/unitarios/organizador.test.js
+++ tests/unitarios/organizador.test.js
@@ -1,10 +1,24 @@
-import { test } from 'node:test' // ejecuta las pruebas
+import { describe, it } from 'node:test' // ejecuta las pruebas
 import assert from 'node:assert' // compara resultados
-import { revisarCarpeta } from '../../src/organizador'
+import path from 'node:path'
+import { revisarCarpeta } from '../../src/organizador.js'
 
-test('Revisar carpetas', async (t) => {
-    await t.test('Lee la carpeta y devuelve solo sus archivos', () => {
-        const archivos = revisarCarpeta('../pruebas')
-        assert.strictEqual(archivos.length, 1) // archivo.txt (excluye la carpeta "otra")
+describe('Revisar carpeta', async () => {
+    await it('Leer la carpeta y obtener sus archivos', async () => {
+        // Arrange
+        const rutaUsuario = path.resolve('./tests/pruebas')
+        // Act
+        const archivos = await revisarCarpeta(rutaUsuario)
+        // Assert
+        assert.equal(archivos.length, 2) // saltea la carpeta
+        assert.deepStrictEqual(archivos.sort(), ['archivo.pdf', 'texto.txt'].sort()) // compara en profundidad
+        // ordena con .sort() porque readdir no siempre lee de la misma manera
+    })
+
+    await it('Lanzar un error si no se logró leer la carpeta correctamente', async () => {
+        await assert.rejects(revisarCarpeta('carpeta/que/no/existe'), {
+            name: 'Error',
+            message: /^Error al abrir la carpeta:/,
+        })
     })
 })
