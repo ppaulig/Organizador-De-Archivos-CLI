9e12f277aced9d5929e9072af8857f329736249f
Paula <generalpaulawrk@gmail.com>
2025-12-27 20:12:24 -0300

    Test unitario de la función clasificarArchivo

--- tests/unitarios/organizador.test.js
+++ tests/unitarios/organizador.test.js
@@ -1,7 +1,7 @@
 import { describe, it } from 'node:test' // ejecuta las pruebas
 import assert from 'node:assert' // compara resultados
 import path from 'node:path'
-import { revisarCarpeta } from '../../src/organizador.js'
+import { clasificarArchivo, revisarCarpeta } from '../../src/organizador.js'
 
 describe('Revisar carpeta', async () => {
     await it('Leer la carpeta y obtener sus archivos', async () => {
@@ -20,5 +20,21 @@ describe('Revisar carpeta', async () => {
             name: 'Error',
             message: /^Error al abrir la carpeta:/,
         })
+    })
+})
+
+describe('Clasificar archivo', async () => {
+    await it('Clasifica un archivo de texto como "Documentos" según su extensión (.txt)', async () => {
+        // Act
+        const categoria = clasificarArchivo('texto.txt')
+        // Assert
+        assert.equal(categoria, 'Documentos')
+    })
+
+    await it('Clasifica un archivo de audio como "Audio" según su extensión (.mp3)', async () => {
+        // Act
+        const categoria = clasificarArchivo('audio.mp3')
+        // Assert
+        assert.equal(categoria, 'Audio')
     })
 })
